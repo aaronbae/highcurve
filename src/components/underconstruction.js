2a94e821401ba3dddb29681d602af59a8074f50b
Aaron Bae <aaron@aaronbae.com>
2020-08-23 17:59:04 -0700

    created a temporary underconstruction page

---
+++ src/components/underconstruction.js
@@ -0,0 +1,11 @@
+import '../styles/underconstruction.css'
+
+export default function underconstruction() {
+  return (
+    <div className="under-construction-container">
+      <img src="logo-words.svg" alt="HighCurve Logo" className="highcurve-logo"/>
+      <div className="underconstruction-message">Currently, HighCurve is under construction...</div>
+      <div className="underconstruction-date">8.23.2020</div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import Layout from '../components/layout'
+import UnderConstruction from '../components/underconstruction'
 import '../styles/index.css'
 
 export default function Home() {
+  if(process.env.NODE_ENV === "production") {
+    return <UnderConstruction />
+  }
   return (
     <Layout>
       <div id="home">
         HOME
-        {process.env.NODE_ENV}
       </div> 
     </Layout>
   )
