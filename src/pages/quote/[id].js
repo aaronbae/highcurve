6c2f6bffc2548e552f0988c71548211d20207e40
Aaron Bae <aaron@aaronbae.com>
2020-09-08 01:37:27 -0700

    finsihed styling the line chart

--- src/pages/quote/[id].js
+++ src/pages/quote/[id].js
@@ -1,12 +1,28 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import Table from '../../components/table'
+import { 
+  ResponsiveContainer, 
+  ReferenceLine, 
+  XAxis,
+  YAxis, 
+  LineChart, 
+  Tooltip, 
+  Line 
+} from 'recharts'
 import '../../styles/quote/id.css'
 
 export default function Quote() {
   const router = useRouter()
   const { id } = router.query
   const [data, setData] = useState([])
+
+  function getRange(data, key, margin_scale_min=0.2, margin_scale_max=0.2) {
+    const filtered = data.map(item => item[key])
+    const temp = [Math.min(...filtered), Math.max(...filtered)]
+    const range = temp[1] - temp[0]
+    return [temp[0] - range*margin_scale_min, temp[1] + range*margin_scale_max]
+  }
 
   function round(number, float_digits=2) {
     return Math.round(number*Math.pow(10, float_digits))/Math.pow(10, float_digits)
@@ -18,7 +34,25 @@ export default function Quote() {
       .then(response => response.json())
       .then(data => {
         if(data.stocks){
-          setData(data.stocks)
+          const formatted = data.stocks.map(item=>{
+            return {
+              ticker: item.ticker,
+              adj_close: item.adj_close,
+              volume: item.volume,
+              date: item.date.substring(0, 10),
+              change: round(item.change, 4),
+              open: round(item.open),
+              high: round(item.high),
+              low: round(item.low),
+              close: round(item.close), 
+              millisec: new Date(item.date).getTime()
+            } 
+          }) 
+          const dates = formatted.map(item=>item.date)
+          const filtered = formatted.filter((item, index)=>{
+            return dates.indexOf(item.date) === index
+          })
+          setData(filtered)
         }
       })
     }
@@ -26,18 +60,45 @@ export default function Quote() {
 
   return (
     <div className="quote-container">
-      {id &&
-        <div>        
-          <p className="quote-title">{id}</p>
+      {id && data.length > 0 &&
+        <div>
+          <div className="quote-chart-container">
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart data={[...data].reverse()}>
+                <XAxis 
+                  dataKey="millisec" 
+                  type="number" 
+                  domain={getRange(data, "millisec", 0.01, 0.01)} 
+                  hide/>
+                <YAxis type="number" domain={getRange(data, "close", 0.1, 0.4)} hide />
+                <ReferenceLine y={data[data.length-1].close} stroke="gray" strokeDasharray="3 3" />
+                <Tooltip 
+                  separator="" 
+                  formatter={(value, name, props)=>[`$${value}`, ""]}
+                  labelFormatter={milli=>new Date(milli).toLocaleDateString()}
+                  contentStyle={{
+                    border: "none", 
+                    "backgroundColor":"rgba(0, 0, 0, 0)"
+                  }}
+                  itemStyle={{fontSize: "1.7rem"}}
+                  position={{x: 0, y: 0}}/>
+                <Line type="monotone" dataKey="close" stroke="red" />
+              </LineChart> 
+            </ResponsiveContainer>
+          </div>
+          <div className="quote-title">
+            <span>{id}</span>
+            <span>{data[data[0].close]}</span>
+          </div>
           <Table header={["Date", "Change", "Open", "High", "Low", "Close"]}>
             {data.map((item, index)=>
               <tr key={index}>
-                <td>{item.date.substring(0, 10)}</td>
-                <td>{round(item.change, 4)}</td>
-                <td>{round(item.open)}</td>
-                <td>{round(item.high)}</td>
-                <td>{round(item.low)}</td>
-                <td>{round(item.close)}</td>
+                <td>{item.date}</td>
+                <td>{item.change}</td>
+                <td>{item.open}</td>
+                <td>{item.high}</td>
+                <td>{item.low}</td>
+                <td>{item.close}</td>
               </tr>
             )}
           </Table>
