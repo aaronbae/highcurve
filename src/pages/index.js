4b5d06db2a7e9d3d0f6222aaf1c4e89c428efea9
Aaron Bae <aaron@aaronbae.com>
2020-09-08 02:23:49 -0700

    started topmovers

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import UnderConstruction from '../components/underconstruction'
 import '../styles/index.css'
 
@@ -5,6 +6,22 @@ export default function Home() {
   if(process.env.NODE_ENV === "production") {
     return <UnderConstruction />
   }
+  function current() {
+    const days_in_milli = 1000 * 60 * 60 * 24
+    return new Date(Math.floor(Date.now() / days_in_milli) * days_in_milli)
+  }
+  
+  useEffect(()=>{
+    const rounded_date = current()
+    // fetch(`${process.env.NEXT_PUBLIC_STOCKS_URL}topmovers/${rounded_date}/20`) 
+    // .then(response => response.json())
+    // .then(data => {
+    //   if(data.stocks){
+    //     setData(filtered)
+    //   }
+    // })
+  }, [])
+
   return (
     <div id="home">
       Nothing to show
