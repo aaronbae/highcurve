905f7689c0761c712187c30e1848646ab044c3ad
Aaron Bae <aaron@aaronbae.com>
2020-09-10 03:15:44 -0700

    finished disclaimer page

--- src/pages/disclaimer.js
+++ src/pages/disclaimer.js
@@ -3,10 +3,21 @@ import '../styles/disclaimer.css';
 
 export default function Disclaimer() {
   return (
-    <div>
+    <div className="disclaimer-container">
       <Banner height="30vh">
         DISCLAIMER
       </Banner>
+      <p className="disclaimer-title">Disclaimer</p>
+      <div className="disclaimer-section">
+        <p className="disclaimer-content">
+          HighCurve is intended to be used as an auxillary information
+          to an investor's decision. It is recommended that any 
+          investment decision be augmented to the content 
+          presented on HighCurve. By using HighCurve, all users are implicitly 
+          agreeing that HighCurve is not responsible for the outcomes 
+          of an investment decision. 
+        </p>
+      </div>
     </div>
   )
 }
