9b77c9f77ee6cbbc310ff395d23be7987d72e24d
Aaron Bae <aaron@aaronbae.com>
2020-09-10 00:00:00 -0700

    stylized table

--- src/components/table.js
+++ src/components/table.js
@@ -2,18 +2,22 @@ import '../styles/table.css'
 
 export default function Table(params) {
   return (
-    <table className="styled-table">
-      <thead>
-        <tr>
-          {params.header.map((item, index)=>
-            <th key={index}>{item}</th>
-          )}
-        </tr>
-      </thead>
-      <tbody>
-        {params.children}
-      </tbody>
-    </table>
-    
+    <div className="styled-table-container">
+      <table className="styled-table">
+        {params.title && 
+          <caption>{params.title}</caption>
+        }
+        <thead>
+          <tr>
+            {params.header.map((item, index)=>
+              <th key={index}>{item}</th>
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {params.children}
+        </tbody>
+      </table>
+    </div>
   )
 }
