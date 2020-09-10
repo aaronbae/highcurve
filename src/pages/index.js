48a06c80aff067e2232775ded2151d76c96acb70
Aaron Bae <aaron@aaronbae.com>
2020-09-10 00:00:44 -0700

    started the top movers table

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,45 @@
 import { useEffect, useState } from 'react'
+import moment from 'moment'
 import UnderConstruction from '../components/underconstruction'
+import Table from '../components/table'
 import '../styles/index.css'
 
 export default function Home() {
+  const [date, setDate] = useState(moment().startOf("day"))
+  const [data, setData] = useState([]);
   if(process.env.NODE_ENV === "production") {
     return <UnderConstruction />
   }
-  function current() {
-    const days_in_milli = 1000 * 60 * 60 * 24
-    return new Date(Math.floor(Date.now() / days_in_milli) * days_in_milli)
-  }
   
   useEffect(()=>{
-    const rounded_date = current()
-    // fetch(`${process.env.NEXT_PUBLIC_STOCKS_URL}topmovers/${rounded_date}/20`) 
-    // .then(response => response.json())
-    // .then(data => {
-    //   if(data.stocks){
-    //     setData(filtered)
-    //   }
-    // })
+    const rounded_milli = date.toDate().getTime()
+    fetch(`${process.env.NEXT_PUBLIC_STOCKS_URL}topmovers/${rounded_milli}/20`) 
+    .then(response => response.json())
+    .then(data => {
+      if(data.stocks){
+        setData(data.stocks.map(x=>{
+          return {...x, 
+            close: "$" + Math.round(100*x.close)/100, 
+            change: Math.round(10000*x.change)/100 + "%"}
+        }))
+      }
+    })
   }, [])
 
   return (
     <div id="home">
-      Nothing to show
+      <p className="date-header">{date.format("MMMM Do")}</p>
+      <div className="topmovers-container">
+        <Table title="Top Movers" header={["Symbol", "Price", "Change"]}>
+          {data.length > 0 && data.map((item, index)=>
+            <tr key={index}>
+              <td>{item.ticker}</td>
+              <td>{item.close}</td>
+              <td>{item.change}</td>
+            </tr>
+          )}
+        </Table>
+      </div>
     </div>
   )
 }
